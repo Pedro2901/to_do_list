9a4ce747bc5116ed6a390133d3eca59d752d4599
Pedro <ppjimenez@uninorte.edu.co>
2025-08-11 20:00:38 -0500

    cosas nuevas

---
+++ app.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+#include <string>
+
+
+int main(){
+     
+    std::string hello = "look at my truck";
+    
+      std::cout << hello << std::endl;
+      
+    return 0;
+}
+
